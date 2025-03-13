a98c61cf404b1b74328689ba4ce3cf001c5a35dc
Valentyn Zakharenko <vvzakharenko@gmail.com>
2025-03-13 18:12:08 +0200

    Revert "Revert "introduce IIfAble""
    This reverts commit 5b12d07f8b6b2fbb320367c85591ea2f4561e133.

---
+++ Elifir/IIfAble.cs
@@ -0,0 +1,10 @@
+﻿namespace Nuisho.Elifir
+{
+    public interface IIfAble<Prev, I>
+    {
+        ˣ<Prev, ˣIf<I>> If(Func<I, bool> condition);
+
+        ˣ<Prev, ˣIf<I>.Is<Iʹ>> If<Iʹ>(Func<TypeCondition<I, Iʹ>> condition)
+            where Iʹ : I;
+    }
+}
--- Elifir/Some.If.Then.cs
+++ Elifir/Some.If.Then.cs
@@ -2,25 +2,25 @@
 {
     public static partial class Syntax
     {
-        public static ˣ<Some, ˣ.If<I>.Then<Tʹ>> Then<Some, I, T, Tʹ>(
-            this ˣ<Some, ˣ.If<I>.Then<T>> o,
+        public static ˣ<Some, ˣIf<I>.Then<Tʹ>> Then<Some, I, T, Tʹ>(
+            this ˣ<Some, ˣIf<I>.Then<T>> o,
             Func<T, Tʹ> map) =>
                 new(o.Prev, 
                     o.Next.Then(map));
 
-        public static ˣ<ˣ<Some, ˣ.If<I>.Then<T>>, ˣ.If<T>> If<Some, I, T>(
-            this ˣ<Some, ˣ.If<I>.Then<T>> o,
+        public static ˣ<ˣ<Some, ˣIf<I>.Then<T>>, ˣIf<T>> If<Some, I, T>(
+            this ˣ<Some, ˣIf<I>.Then<T>> o,
             Func<T, bool> condition) =>
                 new(o, If(condition));
 
-        public static ˣ<ˣ<Some, ˣ.If<I>.Then<T>>, ˣ.If<T>.Is<Tʹ>> If<Some, I, T, Tʹ>(
-            this ˣ<Some, ˣ.If<I>.Then<T>> o,
+        public static ˣ<ˣ<Some, ˣIf<I>.Then<T>>, ˣIf<T>.Is<Tʹ>> If<Some, I, T, Tʹ>(
+            this ˣ<Some, ˣIf<I>.Then<T>> o,
             Func<TypeCondition<T, Tʹ>> condition)
                 where Tʹ : T =>
                     new(o, If(condition));
 
-        public static ˣ<Some, ˣ.If<I>.Then<T>.Else> Else<Some, I, T>(
-            this ˣ<Some, ˣ.If<I>.Then<T>> o) =>
+        public static ˣ<Some, ˣIf<I>.Then<T>.Else> Else<Some, I, T>(
+            this ˣ<Some, ˣIf<I>.Then<T>> o) =>
                 new(o.Prev, 
                     o.Next.Else());
     }
